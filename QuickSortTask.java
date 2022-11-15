76896d4ccee821194a0afca6b1a500fb24c12bb3
unknown <rafikchb@hotmail.fr>
2022-11-15 22:36:32 +0100

    verssion presque fini manque que la choerance

--- QuickSortTask.java
+++ QuickSortTask.java
@@ -1,57 +1,71 @@
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.Future;
 
-class QuickSortTask implements Runnable{
-    public static AtomicInteger cpt = new AtomicInteger(0);
+class QuickSortTask implements Callable<Boolean> {
+    // list des tache en prevu pour etre executer (Futures).
+    public static List<Future<Boolean>> listTask = Collections.synchronizedList(new LinkedList<Future<Boolean>>());
+    // le threadpool au quelle les tache seron affecter 
     private ExecutorService threadPool;
+    // refereance vers le tebleau trier 
     private int[] tableau;
     private int debut;
     private int fin;
 
+    // constructeur a fin de crer un nouvelle tache .
     public QuickSortTask(ExecutorService threadPool, int[] tableau, int debut, int fin) {
-        cpt.incrementAndGet();
-        System.out.println("craeted task nbr = " + cpt.get());
         this.threadPool = threadPool;
         this.tableau = tableau;
         this.debut = debut;
         this.fin = fin;
     }
 
+    // method call du tache
     @Override
-    public void run() {
+    public Boolean call() {
         trierRapidement(threadPool, tableau, debut, fin);
-        QuickSortTask.cpt.decrementAndGet();
+        return true;
     }
-
+    // method trier utiliser par les tache .
     private void trierRapidement(ExecutorService threadPool, int[] t, int debut, int fin) {
-        System.out.println("trierRapidement");
+
         if (debut < fin) {
             int p = partitionner(t, debut, fin);
-
             int taillTabgauche = (p) - debut;
             if (t.length / 100.0 < taillTabgauche) {
-                threadPool.execute(new QuickSortTask(threadPool, t, debut, p - 1));
+                listTask.add(threadPool.submit(new QuickSortTask(threadPool, t, debut, p - 1)));
             } else {
                 trierRapidement(threadPool, t, debut, p - 1);
             }
-
             int taillTabDroit = fin - (p);
             if (t.length / 100.0 < taillTabDroit) {
-                threadPool.execute(new QuickSortTask(threadPool, t, p + 1, fin));
+
+                threadPool.submit(new QuickSortTask(threadPool, t, p + 1, fin));
             } else {
                 trierRapidement(threadPool, t, p + 1, fin);
             }
         }
     }
 
-    private void echangerElements(int[] t, int m, int n) {
+    // method tirer sequentielle utiliser pour faire le trie sequentielle.
+    public static void trierRapidementSequetielle(int[] t, int début, int fin) {
+        if (début < fin) {                             // S'il y a un seul élément, il n'y a rien à faire!
+            int p = partitionner(t, début, fin) ;      
+            trierRapidementSequetielle(t, début, p-1) ;
+            trierRapidementSequetielle(t, p+1, fin) ;
+        }
+    }
+
+    private static  void echangerElements(int[] t, int m, int n) {
         int temp = t[m];
         t[m] = t[n];
         t[n] = temp;
     }
 
-    private int partitionner(int[] t, int début, int fin) {
+    private static int partitionner(int[] t, int début, int fin) {
         int v = t[fin]; // Choix (arbitraire) du pivot : t[fin]
         int place = début; // Place du pivot, à droite des éléments déplacés
         for (int i = début; i < fin; i++) { // Parcours du *reste* du tableau
---
+++ TriRapide.java
@@ -0,0 +1,67 @@
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class TriRapide {
+    public static void main(String[] args) throws Exception {
+        int taille = 6;
+        int[] multiTachTab = new int[taille]; // multiTachTab utiliser pour le tri sequentielle.
+        int[] sequentielTab = new int[taille]; // multiTachTab utiliser pour le trie multiTache.
+        int borne = 10 * taille;
+        // initialisation du multiTachTab.
+        Random aléa = new Random();
+        for (int i = 0; i < taille; i++) {
+            sequentielTab [i] = multiTachTab[i] = aléa.nextInt(2 * borne) - borne;
+        }
+       
+        // affichage du tableau initial .
+        System.out.print("tableau initial : ");
+        afficher(multiTachTab, 0, taille - 1);
+
+        // debut de la partie sequentielle
+        Long debutDuTri = System.nanoTime();
+        QuickSortTask.trierRapidementSequetielle(sequentielTab, 0, taille - 1);
+        Long finDuTri = System.nanoTime();
+        Long dureeDuTriSequentiel = (finDuTri - debutDuTri) / 1_000_000;
+        System.out.println("Version séquentielle : " + dureeDuTriSequentiel + " ms .");
+        System.out.print("Tableau trié : ");
+        afficher(sequentielTab, 0, taille - 1);
+        // fin de la partie sequentielle.
+
+        // debut de la partie parall`ele
+        // creation du threadpool de 4 thread.
+        ExecutorService threadPool = Executors.newFixedThreadPool(4); // creation du threadpool
+        debutDuTri = System.nanoTime();
+        // lancement du tri.
+        QuickSortTask.listTask.add(threadPool.submit(new QuickSortTask(threadPool, multiTachTab, 0, taille - 1)));
+        // attendre que toute les tache termine .
+        while (!QuickSortTask.listTask.isEmpty()) {
+            Future<Boolean> future = QuickSortTask.listTask.remove(0);
+            future.get();
+        }
+        // arret du thread pool
+        threadPool.shutdown();
+        finDuTri = System.nanoTime();
+        Long dureeDuTriParallele = (finDuTri - debutDuTri) / 1_000_000;
+        System.out.println("Version paralléle " + dureeDuTriParallele + " ms.");
+        System.out.print("Tableau trié : ");
+        afficher(multiTachTab, 0, taille - 1);
+        System.out.println("Gain observé : " + (dureeDuTriSequentiel*1.0/dureeDuTriParallele) );
+       
+        // TODO : verifier si les deux tableau sont choerant .
+     
+
+    }
+
+    private static void afficher(int[] t, int début, int fin) {
+        for (int i = début; i <= début + 3; i++) {
+            System.out.print(" " + t[i]);
+        }
+        System.out.print("...");
+        for (int i = fin - 3; i <= fin; i++) {
+            System.out.print(" " + t[i]);
+        }
+        System.out.println();
+    }
+}
